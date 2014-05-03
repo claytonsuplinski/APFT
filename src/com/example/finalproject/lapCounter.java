8ec9c6c3bf9918e35be511cb771f2f257e116fa2
Clayton Suplinski <suplinsk@mumble-23.cs.wisc.edu>
2014-05-03 11:02:40 -0500

    Worked on LapCounter

--- src/com/example/finalproject/lapCounter.java
+++ src/com/example/finalproject/lapCounter.java
@@ -11,38 +11,98 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 public class lapCounter extends Activity implements View.OnClickListener {
+	
+	static int UNDO_ID = 9999;
+	static int START_ID = 99999;
+	static int STOP_ID = 999999;
+	static int TIMER_ID = 9999999;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lap_counter);
 
-		int numCadets = 25;
+		int numCadets = 24;
 
 		TableLayout cadets = (TableLayout)findViewById(R.id.tableLayout2);
 		cadets.setStretchAllColumns(true);
 		cadets.bringToFront();
 		TableRow tr =  new TableRow(this);
 		for(int i = 0; i < numCadets; i++){
-			if(i%2 == 0){
-				tr =  new TableRow(this);
-			}
 			TextView c1 = new TextView(this);
 			c1.setBackgroundResource(R.drawable.gradient);
 			
 			c1.setId(i);
 			c1.setOnClickListener(this);
-			c1.setText("Soldier " + (i+1) + ": " + (i+3));
+			
+			c1.setText("Soldier " + (i+1) + ": 14");
 			c1.setTextSize(24);
 			c1.setTextColor(Color.WHITE);
-			c1.setGravity((i%2 == 0 ? Gravity.LEFT : Gravity.RIGHT));
+			c1.setGravity(Gravity.CENTER);
 			tr.addView(c1);
 			
 			tr.setGravity(Gravity.CENTER);	
 			if(i%2 == 1){
 				cadets.addView(tr);
+				tr =  new TableRow(this);
 			}
 		}
+		cadets.addView(tr);
+		
+		tr =  new TableRow(this);
+		TextView c1 = new TextView(this);
+		c1.setText("\n");
+		c1.setTextSize(18);
+		tr.setGravity(Gravity.CENTER);
+		tr.addView(c1);
+		cadets.addView(tr);
+		
+		tr =  new TableRow(this);
+		c1 = new TextView(this);
+		c1.setBackgroundResource(R.drawable.gradient);		
+		c1.setId(UNDO_ID);
+		c1.setOnClickListener(this);
+		c1.setText("Undo");
+		c1.setTextSize(20);
+		c1.setTextColor(Color.WHITE);
+		c1.setGravity(Gravity.CENTER);
+		tr.addView(c1);
+		
+		c1 = new TextView(this);
+		c1.setBackgroundResource(R.drawable.gradient);		
+		c1.setId(START_ID);
+		c1.setOnClickListener(this);
+		c1.setText("Start");
+		c1.setTextSize(20);
+		c1.setTextColor(Color.WHITE);
+		c1.setGravity(Gravity.CENTER);
+		tr.addView(c1);
+		tr.setGravity(Gravity.CENTER);
+		cadets.addView(tr);
+		
+		tr =  new TableRow(this);
+		c1 = new TextView(this);
+		c1.setBackgroundResource(R.drawable.gradient2);		
+		c1.setId(TIMER_ID);
+		c1.setOnClickListener(this);
+		c1.setText("0:00");
+		c1.setTextSize(20);
+		c1.setTextColor(Color.BLACK);
+		c1.setGravity(Gravity.CENTER);
+		tr.addView(c1);
+		
+		c1 = new TextView(this);
+		c1.setBackgroundResource(R.drawable.gradient);		
+		c1.setId(STOP_ID);
+		c1.setOnClickListener(this);
+		c1.setText("Stop");
+		c1.setTextSize(20);
+		c1.setTextColor(Color.WHITE);
+		c1.setGravity(Gravity.CENTER);
+		tr.addView(c1);
+		tr.setGravity(Gravity.CENTER);
+		cadets.addView(tr);
+		
 	}
 
 	@Override
