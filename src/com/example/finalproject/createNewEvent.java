4c4d5b1ed1a5ae348afe7db39abf5396221bf11a
Alexander Durgin <alexander@Lambda.(none)>
2014-05-05 15:37:17 -0500

    updated createNewEvent frame

--- src/com/example/finalproject/createNewEvent.java
+++ src/com/example/finalproject/createNewEvent.java
@@ -1,7 +1,12 @@
 package com.example.finalproject;
 
+import com.parse.ParseException;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class createNewEvent extends Activity {
 
@@ -11,4 +16,16 @@ public class createNewEvent extends Activity {
 		setContentView(R.layout.activity_create_new_event);
 	}
 	
+	public void onButtonClick(View v) throws ParseException
+	{
+		switch(v.getId())
+		{
+		case R.id.button_saveNewEvent:
+			EditText editDate = (EditText)findViewById(R.id.inputNewEvent);
+			varApplication va = (varApplication)getApplicationContext();
+			String dbId = va.getId();
+			DBUtil.addEvent(dbId, editDate.getText().toString());
+		default:
+		}
+	}
 }
